12f7fb5d39ebc81059a46ce4ea6fb5c6c0abcdca
kreal <xukzhen@hotmail.com>
2022-03-14 00:07:22 -0400

    create model and model folder

---
+++ app/src/main/java/com/example/easysublet/model/User.java
@@ -0,0 +1,51 @@
+package com.example.easysublet.model;
+
+import androidx.annotation.Nullable;
+
+public class User {
+
+    @Nullable
+    private String username, email, password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Nullable
+    public String getUsername() {
+        return username;
+    }
+
+    @Nullable
+    public String getEmail() {
+        return email;
+    }
+
+    @Nullable
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Nullable
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Nullable
+    public boolean passwordIsCorrect(String password) {
+        return this.password.equals(password);
+    }
+
+    @Nullable
+    public boolean setPassword(String oldPassword, String newPassword) {
+        if(!passwordIsCorrect(oldPassword)) {
+            return false;
+        }
+        this.password = newPassword;
+        return true;
+    }
+
+
+}
