764d08ebcb87f4314865b0356939b04fde8ccd71
Xiaodan <zhaoxiaodan.xd@gmail.com>
2022-04-02 22:04:18 -0400

    Add ui for add home post and add roommate post

--- app/src/main/java/com/example/easysublet/view/AddRoommatePostFragment.java
+++ app/src/main/java/com/example/easysublet/view/AddRoommatePostFragment.java
@@ -5,15 +5,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.easysublet.databinding.FragmentAddHomePostBinding;
 import com.example.easysublet.databinding.FragmentAddRoommatePostBinding;
 import com.example.easysublet.viewmodel.AddPostViewModel;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.DateValidatorPointForward;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class AddRoommatePostFragment extends Fragment {
 
@@ -33,6 +43,36 @@ public class AddRoommatePostFragment extends Fragment {
 
         final TextView textView = binding.text;
         addPostViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        TextInputLayout dateTextInput = binding.tilTimePicker;
+        EditText dateEditText = dateTextInput.getEditText();
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.clear();
+        long today = MaterialDatePicker.todayInUtcMilliseconds();
+
+        CalendarConstraints.Builder constraintsBuilder = new CalendarConstraints.Builder();
+        constraintsBuilder.setStart(today);
+        constraintsBuilder.setValidator(DateValidatorPointForward.now());
+
+        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+        builder.setTitleText("SELECT DATE");
+        builder.setCalendarConstraints(constraintsBuilder.build());
+
+        final MaterialDatePicker materialDatePicker = builder.build();
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                materialDatePicker.show(getParentFragmentManager(),"DATE_PICKER");
+            }
+        });
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                dateEditText.setText(materialDatePicker.getHeaderText());
+            }
+        });
+
         return root;
     }
 
