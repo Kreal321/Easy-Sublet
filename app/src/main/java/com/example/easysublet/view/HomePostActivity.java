f4cab9046ee88063838895dc92795bfe8a767599
Xiaodan <zhaoxiaodan.xd@gmail.com>
2022-04-04 20:35:08 -0400

    UI for home post

--- app/src/main/java/com/example/easysublet/view/HomePostActivity.java
+++ app/src/main/java/com/example/easysublet/view/HomePostActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.easysublet.databinding.ActivityHomePostBinding;
 import com.example.easysublet.model.HomePost;
@@ -42,6 +43,20 @@ public class HomePostActivity extends AppCompatActivity {
             public void onChanged(HomePost homePost) {
                 binding.title.setText(homePost.getTitle());
                 binding.image.setImageResource(homePost.getImage());
+                binding.address.setText(homePost.getAddress());
+                binding.timePeriod.setText(homePost.getTime());
+                binding.rent.setText(Integer.toString(homePost.getRent()));
+                binding.bedroomNum.setText(Integer.toString(homePost.getBedroomNum()));
+                binding.bathroomNum.setText(Integer.toString(homePost.getBathroomNum()));
+                binding.furnished.setText(homePost.isFurnished()?"✓":"✗");
+                binding.pet.setText(homePost.getPet()?"✓":"✗");
+                binding.gender.setText(homePost.getGender());
+                binding.contact.setText(homePost.getContact());
+                if (homePost.getOther() == null || homePost.getOther().length() == 0) {
+                    binding.llOther.setVisibility(View.INVISIBLE);
+                } else {
+                    binding.other.setText(homePost.getOther());
+                }
             }
         });
 
