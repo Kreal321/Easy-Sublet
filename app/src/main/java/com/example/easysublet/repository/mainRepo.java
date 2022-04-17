86eb7a24522126039273f0862c718d6293368221
kreal <xukzhen@hotmail.com>
2022-04-17 16:49:23 -0400

    fix bugs

--- app/src/main/java/com/example/easysublet/repository/mainRepo.java
+++ app/src/main/java/com/example/easysublet/repository/mainRepo.java
@@ -147,7 +147,7 @@ public class mainRepo {
                     mutableLiveUserData.postValue(null);
                     // If sign in fails, display a message to the user.
                     Log.w("SignInStatus", "signInWithEmail:failure", task.getException());
-                    Toast.makeText(application, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    // Toast.makeText(application, "Authentication failed.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
