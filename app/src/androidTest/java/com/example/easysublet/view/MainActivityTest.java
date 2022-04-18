14d411e0b6fb4af7c26d46fe2bd2a346689e2956
kreal <xukzhen@hotmail.com>
2022-04-18 11:55:49 -0400

    login test cases

---
+++ app/src/androidTest/java/com/example/easysublet/view/MainActivityTest.java
@@ -0,0 +1,61 @@
+package com.example.easysublet.view;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+import android.view.View;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.easysublet.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+    public View decorView;
+
+    @Before
+    public void setUp(){
+        activityScenarioRule.getScenario().onActivity(new ActivityScenario.ActivityAction<MainActivity>() {
+            @Override
+            public void perform(MainActivity activity) {
+                decorView = activity.getWindow().getDecorView();
+            }
+        });
+    }
+
+    @Test
+    public void testLoginFailed(){
+        onView(withId(R.id.emailEntry)).perform(typeText("test@1.com"),closeSoftKeyboard());
+        onView(withId(R.id.passwordEntry)).perform(typeText("thisiswrong"),closeSoftKeyboard());
+        onView(withId(R.id.loginBtn)).perform(click());
+        onView(withText(R.string.login_error)).inRoot(withDecorView(not(decorView))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testLogin(){
+        onView(withId(R.id.emailEntry)).perform(typeText("test@1.com"),closeSoftKeyboard());
+        onView(withId(R.id.passwordEntry)).perform(typeText("123456"),closeSoftKeyboard());
+        onView(withId(R.id.loginBtn)).perform(click());
+        onView(withText(R.string.login_success)).inRoot(withDecorView(not(decorView))).check(matches(isDisplayed()));
+    }
+
+}
